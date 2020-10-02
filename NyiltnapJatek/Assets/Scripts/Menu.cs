cc95abf97b9916f059a537e7345d09d9dc077f1e
ifod04 <65920825+ifod04@users.noreply.github.com>
2020-10-02 21:30:41 +0200

    aaaa

---
+++ NyiltnapJatek/Assets/Scripts/Menu.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class Menu : MonoBehaviour
+{
+    public enum Scenes {mainMenu, sampleScene, zoldMap };
+    [SerializeField] private Button[] buttons = new Button[3];
+    [SerializeField] private GameObject menuImg;
+    private int index = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuImg.SetActive(!menuImg.activeSelf);
+
+        }
+        if (menuImg.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                buttons[index].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+                index++;
+                if (index > 2)
+                {
+                    index--;
+                }
+                buttons[index].GetComponent<Image>().color = new Color(0.1f, 0.3f, 0.4f);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                buttons[index].GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f);
+                index--;
+                if (index < 0)
+                {
+                    index++;
+                }
+                buttons[index].GetComponent<Image>().color = new Color(0.1f, 0.3f, 0.4f);
+            }
+
+        }
+    }
+
+
+
+    private void Starts()
+    {
+        menuImg.SetActive(!menuImg.activeSelf);
+    }
+
+    private void Credits()
+    {
+
+    }
+
+    private void Exit()
+    {
+        SceneManager.LoadScene((int)Scenes.mainMenu);
+    }
+
+    
+
+
+}
