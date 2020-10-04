ee1626cb569c0f79c3d48f3391b5350bc2bb3638
ifod04 <65920825+ifod04@users.noreply.github.com>
2020-10-04 15:40:49 +0200

    Jegyrendszer

--- NyiltnapJatek/Assets/Scripts/Grade.cs
+++ NyiltnapJatek/Assets/Scripts/Grade.cs
@@ -1,30 +1,19 @@
 ﻿using UnityEngine;
 
-public enum gradeEnum { one = 1, two, three, four, five };
-
 public class Grade : MonoBehaviour
 { 
-    public GameObject coin = default;
-    public gradeEnum nem = default;
-    public static int count = 0;
-    public static int sum = 0;
+    public gradeAllSum.gradeEnum nem = default;
     
     [System.Obsolete]
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponentInParent<Player>())
         {
-            Grade.count++;
-            Grade.sum += (int)nem;
+            gradeAllSum.count++;
+            gradeAllSum.sum += (int)nem;
         }
         Destroy(this.gameObject);
     }
 
 
-}
-
-
-public class gradeWrapper
-{
-    public Grade[] gradeArr = new Grade[3];
 }
