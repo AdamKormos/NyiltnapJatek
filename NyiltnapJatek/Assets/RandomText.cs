e98274d16346c87dac3f4ae9a9472b339bb0a9c7
ifod04 <65920825+ifod04@users.noreply.github.com>
2020-10-13 18:52:15 +0200

    guitarhos

---
+++ NyiltnapJatek/Assets/RandomText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomText : MonoBehaviour
+{
+    public static Color[] pointColors = new Color[5];
+    public static Text txt = default;
+    public static int pointCount = 0;
+    public static int multiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = transform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NyiltnapJatek/Assets/StringNote.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class StringNote : MonoBehaviour
+{
+    private Rigidbody2D rbd = default;
+    public int point = 0;
+
+    private void Start()
+    {
+        switch (point)
+        {
+            //pointes ugyek
+        }
+        rbd = transform.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        rbd.AddForce(new Vector2(0f, -10f * Time.deltaTime), ForceMode2D.Force);
+    }
+}
