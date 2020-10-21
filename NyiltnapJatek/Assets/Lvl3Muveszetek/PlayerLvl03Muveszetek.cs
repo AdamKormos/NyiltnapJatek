353bb76f503a8d1e2ff1f7245fbbb40609ceb880
ifod04 <65920825+ifod04@users.noreply.github.com>
2020-10-21 18:27:19 +0200

    a

--- NyiltnapJatek/Assets/Lvl3Muveszetek/PlayerLvl03Muveszetek.cs
+++ NyiltnapJatek/Assets/Lvl3Muveszetek/PlayerLvl03Muveszetek.cs
@@ -3,57 +3,51 @@ using UnityEngine;
 
 public class PlayerLvl03Muveszetek : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rbd = default;
     [SerializeField] private int index = 0; 
     [SerializeField] private float kottaGap = 8f;
     [SerializeField] private float waitSecond = 12f;
 
     private float i = 0f;
 
-    private bool isMozog = false;
     private bool altUp = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rbd = GetComponent<Rigidbody2D>();
         waitSecond = 1f / waitSecond;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyUp(KeyCode.LeftAlt))
         {
             altUp = true;
-            isMozog = false;
             StartCoroutine(move((float)index - i));
         }
-        else if (Input.GetKey(KeyCode.W) && index != 5)
+        if (!altUp)
         {
-            StartCoroutine(move(kottaGap));
-        }
-        else if (Input.GetKey(KeyCode.S) && index != 0)
-        {
-            StartCoroutine(move(-kottaGap));
+            if (Input.GetKey(KeyCode.W) && index != 5)
+            {
+                StartCoroutine(move(kottaGap));
+            }
+            else if (Input.GetKey(KeyCode.S) && index != 0)
+            {
+                StartCoroutine(move(-kottaGap));
+            }
         }
 
     }
 
     IEnumerator move(float num)
-    {
-        if (!isMozog)
+    { 
+        if (!altUp) num /= 2;
+        for (i = 0f; i < num && !altUp; i += 0.5f)
         {
-            if (!altUp) num /= 2;
-            altUp = false;
-            isMozog = true;
-            for (i = 0f; i < num && !altUp; i += 0.5f)
-            {
-                transform.position += new Vector3(num, 0f);
-                yield return new WaitForSeconds(waitSecond);
-            }
-            isMozog = false;
+            transform.position += new Vector3(num, 0f);
+            yield return new WaitForSeconds(waitSecond);
         }
-        yield return new WaitForSeconds(0.05f);
+        altUp = false;
     }
 }
