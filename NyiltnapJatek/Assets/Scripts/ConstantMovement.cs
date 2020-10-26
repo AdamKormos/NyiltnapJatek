5ffbb8de73274baf2074a62b9d02bb42771ec8e2
AdamKormos <49873113+AdamKormos@users.noreply.github.com>
2020-10-26 18:40:20 +0100

    .

---
+++ NyiltnapJatek/Assets/Scripts/ConstantMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 movePerFrame = default;
+    bool isOnScreen = false;
+
+    private void OnBecameVisible()
+    {
+        isOnScreen = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        if (isOnScreen) Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOnScreen) transform.position += movePerFrame * Time.deltaTime;
+    }
+}
