0052b9cbc14e6270c3764ee9bbc2f80698c9d180
AdamKormos <49873113+AdamKormos@users.noreply.github.com>
2021-05-23 15:45:10 +0200

    .

--- NyiltnapJatek/Assets/Scripts/GameUI.cs
+++ NyiltnapJatek/Assets/Scripts/GameUI.cs
@@ -243,6 +243,7 @@ public class GameUI : MonoBehaviour
             ToggleChildren(levelCompletionPanelText.transform.parent.gameObject, false);
         }
     }
+<<<<<<< HEAD
 
     /// <summary>
     /// Called to load the UI texts using the correct language.
@@ -258,4 +259,6 @@ public class GameUI : MonoBehaviour
             DestroyImmediate(text);
         }
     }
+=======
+>>>>>>> parent of 9bf71b27 (Push before localization feature just in case)
 }
